ab3da24495703a6bd206b29d7f60acb329871e4d
michellesylee <michellebellelee@yahoo.com>
2023-03-02 19:21:41 -0800

    creating example landing page

---
+++ src/client/components/Description.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+export default function Description() {
+  return (
+    <div>
+      <h1>C-React</h1>
+      <h2> Visualize your components on the webpage</h2>
+      <div className="about">
+        <div className="team">
+          <li>Functionality: </li>
+          <li>Add C-React to Chrome Dev Tools</li>
+          <li>Install C-React NPM package</li>
+          <li>Run your application</li>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+}
